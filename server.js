ae8ef3293cdb1f2f0e394114d2a143dfd3d858b6
Prince Tiwari <princetiwari@Princes-MacBook-Air.local>
2026-05-25 23:01:27 +0530

    updated server

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+
 const db = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://ecommerce-frontend-rust-seven.vercel.app"
+}));
 app.use(express.json());
-app.use("/api/products",productRoutes);
-app.use("/images", express.static("images"));
-app.use("/api/auth", authRoutes);
-app.use("/api/orders",orderRoutes);
-app.use("/api/contact",contactRoutes);
 
+
+app.use("/images", express.static("images"));
+
+app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/contact", contactRoutes);
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
