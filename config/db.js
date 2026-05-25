bb28214fd6d606643ef2b750213d8745177a6b5d
Prince Tiwari <princetiwari@Princes-MacBook-Air.local>
2026-05-25 23:36:46 +0530

    fixed railway public url

--- config/db.js
+++ config/db.js
@@ -21,13 +21,7 @@ require("dotenv").config();
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect((err) => {
   if (err) {
