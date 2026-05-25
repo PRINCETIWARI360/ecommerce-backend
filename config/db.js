d3410218495ad9a14fa48a4e42913e730cc1abd1
Prince Tiwari <princetiwari@Princes-MacBook-Air.local>
2026-05-25 23:34:37 +0530

    fixed env

--- config/db.js
+++ config/db.js
@@ -1,3 +1,24 @@
+// const mysql = require("mysql2");
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "6203811578Pt",
+//   database: "ecommerce"
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log("Database connection failed:", err);
+//   } else {
+//     console.log("Database Connected Successfully");
+//   }
+// });
+
+// module.exports = db;
+
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
