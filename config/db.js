961201bfd388c9dc742f2cd3cfddac6d1c28d9ad
Prince Tiwari <princetiwari@Princes-MacBook-Air.local>
2026-05-25 23:16:39 +0530

    connected railway mysql

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "6203811578Pt",
-  database: "ecommerce"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed:", err);
+    console.log("Database Connection Failed", err);
   } else {
-    console.log("Database Connected Successfully");
+    console.log("MySQL Connected");
   }
 });
 
